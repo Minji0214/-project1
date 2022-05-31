ede792f0456a2bb03cb8595b16f5368a97a80351
Minji Jeon <minji799@gmail.com>
2022-05-31 18:28:24 +0900

    first study project

---
+++ static/main.js
@@ -0,0 +1,15 @@
+const emailInput = document.querySelector('.email');
+const alertBox = document.querySelector('.alertemail');
+const inputForm = document.querySelector('.input-box')
+const btn = document.querySelector('.btn')
+const facebook = document.getElementById("facebook")
+const facebook_classname="fa-facebook";
+function emailSubmit (event){
+   event.preventDefault();
+   const email = emailInput.value;
+   emailInput.value ="";
+   if((email.includes("@"))===false)
+   {alertBox.innerHTML = `이메일을 입력해주세요`} else{alert(`${email}`)}
+}
+
+inputForm.addEventListener("submit",emailSubmit)
