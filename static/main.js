2d5c70902b59e68919442999e5d6f0ceb09d2a7e
Minji Jeon <minji799@gmail.com>
2022-06-02 00:01:19 +0900

    네번째 생성완료

--- static/main.js
+++ static/main.js
@@ -2,26 +2,33 @@ const emailInput = document.querySelector('.email');
 const alertBox = document.querySelector('.alertemail');
 const inputForm = document.querySelector('.input-box')
 const btn = document.querySelector('.btn')
-const facebook = document.getElementById("facebook")
-const facebook_classname="fa-facebook";
 
-function emailSubmit (event){
-   event.preventDefault();
-   const email = emailInput.value;
-   emailInput.value ="";
-   if((email.includes("@"))===false)
-   {alertBox.innerHTML = `이메일을 입력해주세요`} else{alert(`${email}`)}
-if((email.includes("@"))===true){
-   $.ajax({
-    type: 'POST',
-    url: '/subscribe',
-    data: {email_give: email},
-    success: function (response) {
-        alert(response['msg'])
+function emailSubmit(event) {
+    event.preventDefault();
+    const email = emailInput.value;
+    emailInput.value = "";
+    if ((email.includes("@")) === false) {
+        colorChange()
+    } else {
+        alert(`${email}`)
     }
-})
-}}
+    if ((email.includes("@")) === true) {
+        $.ajax({
+            type: 'POST',
+            url: '/subscribe',
+            data: {
+                email_give: email
+            },
+            success: function (response) {
+                alert(response['msg'])
+            }
+        })
+    }
+}
 
-inputForm.addEventListener("submit",emailSubmit)
+inputForm.addEventListener("submit", emailSubmit)
 
-
+function colorChange() {
+    emailInput.style.borderColor = "tomato";
+    alertBox.innerHTML = `이메일을 입력해주세요`;
+}
